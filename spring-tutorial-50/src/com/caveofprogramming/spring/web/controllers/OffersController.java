f8bc6cece3f9f92b3edcfac8e22ccf967e1d5b05
Rahul John Louis <rahul.louis@gmail.com>
2016-04-14 21:05:43 -0400

    Configured using JNDI with MySQL DB

--- spring-tutorial-50/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ spring-tutorial-50/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,14 +1,27 @@
 package com.caveofprogramming.spring.web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OffersController {
 
+//	@RequestMapping("/")
+//	public String showHome(HttpSession session)
+//	{
+//		session.setAttribute("name", "Rahul");
+//		return "home";
+//	}
+	
 	@RequestMapping("/")
-	public String showHome()
+	public ModelAndView showHome()
 	{
-		return "home";
+		ModelAndView mv = new ModelAndView("home");
+		Map<String,Object>model=mv.getModel();
+		model.put("name", "<b>Rahul</>");
+		return mv;
 	}
 }
