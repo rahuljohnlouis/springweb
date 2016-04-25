019b177ef85cf880ee63664a037e91c9cf6bd861
Rahul John Louis <rahul.louis@gmail.com>
2016-04-24 20:53:18 -0400

    Added creating custom annotation

--- spring-tutorial-50/src/com/caveofprogramming/spring/web/dao/Offer.java
+++ spring-tutorial-50/src/com/caveofprogramming/spring/web/dao/Offer.java
@@ -4,13 +4,16 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.caveofprogramming.spring.web.validation.ValidEmail;
+
 public class Offer {
 
 	private int id;
 	@Size(min=5,max=100,message="Name must be between 5 and 100")
 	private String name;
 	@NotNull
-	@Pattern(regexp=".*\\@.*\\..*",message="This does not appear to be a valid email")
+	//@Pattern(regexp=".*\\@.*\\..*",message="This does not appear to be a valid email")
+	@ValidEmail(min=6,message="This email nada valid") 
 	private String email;
 	@Size(min=5,max=100,message="text must be between 20 and 255")
 	private String text;
